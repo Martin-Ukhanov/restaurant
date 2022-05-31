32b7bd23805f52f005928d933546a1e7a5175254
Martin <martin.ukhanov@gmail.com>
2022-05-30 23:04:42 -0400

    Added to contact tab

--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,57 @@ const createContact = () => {
     const contact = document.createElement('div');
     contact.classList.add('contact');
 
+    // Contact info
+    const contactInfo = document.createElement('div');
+    contactInfo.classList.add('contact-info');
+
+    const phone = document.createElement('h1');
+    phone.textContent = 'Phone: 647-283-1073';
+
+    const email = document.createElement('h1');
+    email.textContent = 'Email: ramenrules@gmail.com';
+
+    contactInfo.appendChild(phone);
+    contactInfo.appendChild(email);
+
+    // Contact form
+    const contactForm = document.createElement('form');
+    contactForm.classList.add('contact-form');
+
+    const formHeading = document.createElement('h2');
+    formHeading.textContent = 'Ask us a question.';
+
+    const nameInput = document.createElement('input');
+    nameInput.setAttribute('type', 'text');
+    nameInput.setAttribute('name', 'name');
+    nameInput.setAttribute('placeholder', 'Name');
+    nameInput.required = true;
+
+    const emailInput = document.createElement('input');
+    emailInput.setAttribute('type', 'email');
+    emailInput.setAttribute('name', 'email');
+    emailInput.setAttribute('placeholder', 'Email');
+    emailInput.required = true;
+
+    const questionInput = document.createElement('textarea');
+    questionInput.setAttribute('rows', '10');
+    questionInput.setAttribute('cols', '50');
+    questionInput.setAttribute('placeholder', 'Question');
+    questionInput.required = true;
+
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.textContent = 'Submit';
+
+    contactForm.appendChild(formHeading);
+    contactForm.appendChild(nameInput);
+    contactForm.appendChild(emailInput);
+    contactForm.appendChild(questionInput);
+    contactForm.appendChild(submitButton);
+
+    contact.appendChild(contactInfo);
+    contact.appendChild(contactForm);
+
     return contact;
 }
 
